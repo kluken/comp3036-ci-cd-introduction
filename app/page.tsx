14855877745b8d11195b36830eb6fb84f4215df9
Kieran Luken <k.luken@westernsydney.edu.au>
2025-05-30 01:02:56 +0000

    Add CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main style={{color:"red"}}>
       <h1>sdf</h1>
       <p>My Awesome Blog</p>
       <p>Welcome to a CI/CD-ready blog app!</p>
