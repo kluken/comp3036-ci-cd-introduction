77615e696469e367e7563a4b503746cf503011e8
Kieran Luken <k.luken@westernsydney.edu.au>
2025-05-30 00:40:50 +0000

    Fix typo to fix tests

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>sdf</h1>
-      <p>My Awesome Bldog</p>
+      <p>My Awesome Blog</p>
       <p>Welcome to a CI/CD-ready blog app!</p>
       <p>Welcome to a CI/CD-ready blog app!</p>
     </main>
